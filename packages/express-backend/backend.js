93408bcc42a6c5f72674b3c9f080615c83423eb7
timchen0323 <tchen126@calpoly.edu>
2025-01-20 06:17:42 -0800

    Made getting users by name work

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -30,13 +30,27 @@ const users = {
     ]
 };
 
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 
 app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
     res.send(users);
+  }
 });
 
 app.listen(port, () => {
